dfd23ca120c57da4a259c1cffbab88094a9947c3
unurgunite <senpaiguru1488@gmail.com>
2021-04-18 11:22:35 +0300

    Make an extension file to store extensions for gem

---
+++ lib/extensions/extensions.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# This file is an extension initializer. It means that it includes extension files
+# during initialization and extension methods are visible from everywhere
+require_relative "deep_find"
