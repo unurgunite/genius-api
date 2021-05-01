dc2a0a6b05963ecaa932656951b93f31c8e5266d
unurgunite <senpaiguru1488@gmail.com>
2021-05-01 19:11:54 +0300

    Update search module interface

--- lib/genius/api/search.rb
+++ lib/genius/api/search.rb
@@ -1,13 +1,43 @@
+# frozen_string_literal: true
+
 module Genius # :nodoc:
   # +Genius::Search+ module provides methods to work with Genius search database
   module Search
     class << self
+      include Genius::Errors
+
       # +Genius::Search.search+     -> value
+      # @param [nil] token
+      # @param [nil] query
+      # @param [Object] search_by
+      # @return [nil]
       # This method is a standard Genius API {method}[https://docs.genius.com/#search-h2] and it is
       # needed to send a request to the server and get information about artists, tracks and everything
-      # else that may be inside the response body.
-      def search(token = nil, query: nil)
-        nil
+      # else that may be inside the response body. According to https://docs.genius.com/#search-h2, token
+      # is required to be in response, but you can use this method without use of it! =)
+      #
+      # *Examples:*
+      #     Genius::Search.search(query: "Ariana Grande") #=> {..., "full_title"=>" thank u, next by Ariana Grande", ...}
+      #
+      # Also, you can use this method with +search_by+ param, which is needed to search interested
+      # data through returned +JSON+. It uses +deep_find+ extension under the hood.
+      #
+      # *Examples:*
+      #     Genius::Search.search(query: "Bones", search_by: "title") #=> ["Dirt", "HDMI", "RestInPeace", "Sodium", "CtrlAltDelete", "Sixteen", "WhereTheTreesMeetTheFreeway", "Corduroy", "DeadBoy", "WeDontBelieveYou"]
+      #
+      # See Hash#deep_find
+      def search(token: nil, query: nil, search_by: nil)
+        Genius::Auth.authorized?("#{Module.nesting[1].name}.#{__method__}") if token.nil?
+        response = HTTParty.get("https://api.genius.com/search?q=#{query}&access_token=#{token || Genius::Auth.__send__(:token)}").body
+        search = JSON.parse(response)
+        if search_by
+          search.deep_find(search_by)
+        else
+          search
+        end
+      rescue GeniusDown, TokenError, TokenMissing => e
+        puts "Error description: #{e.msg}"
+        puts "Exception type: #{e.exception_type}"
       end
     end
   end
