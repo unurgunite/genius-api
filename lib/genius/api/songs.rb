3c205a0e139189a76440e95da06c843954368de7
unurgunite <senpaiguru1488@gmail.com>
2021-05-12 18:21:18 +0300

    Add `Genius::Songs` module

---
+++ lib/genius/api/songs.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Genius # :nodoc:
+  # +Genius::Songs+ module provides methods to work with songs (lyrics/descriptions/etc.)
+  module Songs
+    class << self
+      include Genius::Errors
+
+    end
+  end
+end
