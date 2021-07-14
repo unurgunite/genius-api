d252c1f455871822f93d1e3c79c3e7a4fea510eb
unurgunite <senpaiguru1488@gmail.com>
2021-07-14 20:15:33 +0300

    Refactor `String#unescape` method

--- lib/extensions/unescape.rb
+++ lib/extensions/unescape.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class String # :nodoc:
-  def unescape(string)
-    string = string.gsub(/(?<!\\)(\\\")/, "\"")
-    string = string.gsub(/(?<!\\)(\\\\\")/, "\\\"")
-    string
+  # String#unescape method unescapes input JSON strings
+  # @return [String (frozen)]
+  def unescape
+    string = gsub(/(?<!\\)(\\\")/, "\"")
+    string.gsub(/(?<!\\)(\\\\\")/, "\\\"")
   end
 end
