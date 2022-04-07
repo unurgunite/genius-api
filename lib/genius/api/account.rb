98075822ce04e50680861a99905807aea8878ee8
unurgunite <senpaiguru1488@gmail.com>
2022-04-08 00:24:33 +0300

    Made `Genius::Account` as a start point for refactoring code semantics. Added `return` where necessary

--- lib/genius/api/account.rb
+++ lib/genius/api/account.rb
@@ -14,39 +14,14 @@ module Genius
       # @return [nil] if TokenError exception raised.
       # This method is a standard Genius API {request}[https://docs.genius.com/#search-h2] to get
       # account info. Output +JSON+ is translated to Hash structure to make it easy to work with account fields.
-      # You can also access to some fields of output hash with +field+ param, which is +nil+ by default. For e.g.,
-      #
-      # @example
-      #     Genius::Account.account(field: "name") #=> "Foo Bar"
-      #
-      # Due to the nesting of a hash there could be multiple keys with the same name, so method will
-      # return an array of values, but if multiple values are the same, method will return
-      # value only once, without storing it in array. For e.g.,
-      #
-      # @example
-      #     Genius::Account.account(field: "id") #=> [100033, 234411]
-      #     Genius::Account.account(field: "url") #=> "https://genius.com/"
       #
       # @example
       #     Genius::Auth.login="yuiaYqbncErCVwItjQxFspNWUZLhGpXrPbkvgbgHSEKJRAlToamzMfdOeDB"
       #     Genius::Account.account #=> {"meta"=>{"status"=>200}, "response"=>{"user"=>{...}}}
-      #
-      # There is a +prettify+ parameter to prettify output hash. It could be called also with +field+ param,
-      # for e.g.:
-      #
-      #     Genius::Account.account(prettify: true) #=>
-      #       {"meta"=>{"status"=>200},
-      #        "response"=>
-      #         {"user"=>
-      #          {"about_me"=>{"dom"=>{"tag"=>"root"}},
-      #           ...
-      #          }}
-      #
-      # But not every output values would be able to be prettified. For e.g.,
-      #
-      #     Genius::Account.account(field: "interactions", prettify: true) #=> { "following" => false }
+      # @todo somehow refactor 50/52 exceptions
       def account(token: nil)
-        Auth.authorized?("#{Module.nesting[1].name}.#{__method__}") if token.nil?
+        return if token.nil? && !Auth.authorized?.nil?
+
         Errors.error_handle(token) unless token.nil?
         response = HTTParty.get("https://api.genius.com/account?access_token=#{token_ext(token)}").body
 
